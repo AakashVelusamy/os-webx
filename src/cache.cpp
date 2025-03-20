bcbcdaf207dc84444efc72a3531d38528524b70e
Aakash Velusamy <aakashvpsgtech@gmail.com>
2025-03-20 11:09:16 +0530

    fix : update cache.cpp

--- src/cache.cpp
+++ src/cache.cpp
@@ -10,12 +10,18 @@ void initialize_cache() {
 
 string get_from_cache(string url) {
     if (cache.find(url) != cache.end()) {
+        cache_order.erase(std::remove(cache_order.begin(), cache_order.end(), url), cache_order.end());
+        cache_order.push_back(url);
         return cache[url];
     }
     return "";
 }
 
 void add_to_cache(string url, string content) {
+    if (cache.find(url) != cache.end()) {
+        cache_order.erase(std::remove(cache_order.begin(), cache_order.end(), url), cache_order.end());
+    }
+
     if (cache.size() >= CACHE_SIZE) {
         string oldest = cache_order.front();
         cache.erase(oldest);
